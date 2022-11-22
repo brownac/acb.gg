3078979d8a5b4a7fd175ab341690cc23c6c38f2e
Austin Brown (K60) <austin.brown@klaviyo.com>
2022-11-22 17:50:12 -0500

    copy change

--- src/copy/home.js
+++ src/copy/home.js
@@ -1,6 +1,6 @@
 export default {
     greeting: "Hi there! I'm a software engineer located in New York City.",
-    currentWork: "I am currently a Software Engineer at Klaviyo on the K60 team. I work mostly in React and Python, but I am also well-versed in Java, PHP, Node.js, and cloud technologies.",
+    currentWork: "I am currently a Senior Software Engineer at Klaviyo on the K60 team. I work mostly in React and Python, but I am also well-versed in Java, PHP, Node.js, and cloud technologies.",
     downloadResume: "download my resume",
     viewMyWork: "view my work",
     contactMe: "contact me via email.",
